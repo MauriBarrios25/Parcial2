969e658f4c4057f9650bd49bb8fa1ae66751fe65
Mau <smis952220@ugb.edu.sv>
2022-04-20 15:43:21 -0600

    subiendo

---
+++ database/migrations/2022_04_20_211731_create_lenguajes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lenguajes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('informacion');
+            $table->string('creador');
+            $table->string('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lenguajes');
+    }
+};
